dcde8ae9b8e25bed93d3f2b37e1aa81b8fdaba61
Victor PC <victor.palomo@fulllab.com.br>
2019-03-09 12:47:00 -0300

    Fix Apipie locale

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,9 @@
 Apipie.configure do |config|
-  config.app_name                = "MinesweeperChallenge"
+  config.app_name                = "Minesweeper"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
+  config.translate               = false
+  config.default_locale          = nil
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
